2f9ce6976f41842d0aa07f9a3874ad8fc930f1fe
Pavel» <dvajdi@bk.ru>
2016-10-03 22:13:24 +0300

    Start

---
+++ src/Start.java
@@ -0,0 +1,7 @@
+import views.Dashboard;
+
+public class Start {
+  public static void main(String[] args) {
+    new Dashboard("Начало");
+  }
+}
---
+++ src/views/Dashboard.java
@@ -0,0 +1,17 @@
+package views;/*
+ Created by Павел on 03.10.2016.
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+  public Dashboard(String title) throws HeadlessException {
+    super(title);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    this.setSize(300,300);
+    this.setLocationRelativeTo(null);
+    this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+  }
+}
