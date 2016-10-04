4b3eeb6279f16bef44aa93bf2764edd2ee4c3a52
Pavel» <dvajdi@bk.ru>
2016-10-04 22:14:40 +0300

    Start

--- src/views/Dashboard.java
+++ src/views/Dashboard.java
@@ -10,25 +10,19 @@ public class Dashboard extends JFrame {
     super(title);
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     this.setVisible(true);
-    this.setSize(500,500);
+    this.setSize(1000,500);
     this.setLocationRelativeTo(null);
     //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+
     DrawPanel dp = new DrawPanel();
+    this.getContentPane().add(BorderLayout.WEST,dp);
+    DrawPanelController dpc = new DrawPanelController(dp,10);
+
     JButton btnRepaint = new JButton("Перерисовать");
-
-    BorderLayout bl = new BorderLayout();
-
-    bl.addLayoutComponent(BorderLayout.CENTER,dp);
-    bl.addLayoutComponent(BorderLayout.WEST,btnRepaint);
-
-    this.getContentPane().setLayout(bl);
-
-
-
     this.getContentPane().add(BorderLayout.EAST,btnRepaint);
 
-    btnRepaint.addActionListener(new DrawPanelController(dp,10));
+    btnRepaint.addActionListener(dpc);
 
 
   }
